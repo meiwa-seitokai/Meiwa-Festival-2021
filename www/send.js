cdd8ee086f09a2f7b927d19d700989ac8509248a
takutuki <tukimidaihuku0605@gmail.com>
2020-12-26 13:27:22 +0900

    ニフクラを導入して予約を送信するところまで

--- www/send.js
+++ www/send.js
@@ -1,9 +1,20 @@
-console.log("loaded");
-let reserve = 0;
+const HR = "101HR";
+const destination = "Reserve_" + HR;
+let reserveNum = 0;
+
+window.onload = function loaded() {
+ console.log("loaded");
+ let Logining = document.querySelector("ons-list-header[id=LoginNow]");
+ Logining.innerHTML = HR;  //ログイン状況
+};
+
 function send() {
-  reserve++;
-  console.log(reserve);
-  let showReserve = document.querySelector('ons-list-item[id="101HR"]');        //101HRの予約内容を取得
-  showReserve = showReserve.textContent
-  console.log(showReserve);
+ let showReserve = document.querySelector("ons-list-item[id=HR]").textContent;   //101HRの予約内容を取得
+ reserveNum++;
+ console.log(reserveNum);
+ //console.log(showReserve);
+ const Reserve = ncmb.DataStore(destination);
+ const reserve = new Reserve();
+ reserve.set("reserveNum", reserveNum)
+        .save()  //ニフクラに送信
 };
