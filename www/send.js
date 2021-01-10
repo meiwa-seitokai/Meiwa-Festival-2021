0065d41b34e8d3701219d4d50f3eca9ddc08b9d9
takutuki <tukimidaihuku0605@gmail.com>
2021-01-10 22:37:10 +0900

    メニュー画面作成

--- www/send.js
+++ www/send.js
@@ -2,17 +2,23 @@ const HR = "101HR";
 const destination = "Reserve_" + HR;
 let reserveNum = 0;
 
-window.onload = function loaded() {
- console.log("loaded");
- let Logining = document.querySelector("ons-list-header[id=LoginNow]");
- Logining.innerHTML = HR;  //ログイン状況
-};
+document.addEventListener('show', function(event) {
+  var page = event.target;
+
+  if (page.id === 'g_assign_number') {
+   console.log("loaded");
+   let Logining = document.querySelector("ons-list-header[id='LoginNow']");
+   Logining.innerHTML = HR;   //ログイン状況
+   };
+
+});
 
 function send() {
  let showReserve = document.querySelector("ons-list-item[id=HR]").textContent;   //101HRの予約内容を取得
  reserveNum++;
  console.log(reserveNum);
  //console.log(showReserve);
+ 
  const Reserve = ncmb.DataStore(destination);
  const reserve = new Reserve();
  reserve.set("reserveNum", reserveNum)
